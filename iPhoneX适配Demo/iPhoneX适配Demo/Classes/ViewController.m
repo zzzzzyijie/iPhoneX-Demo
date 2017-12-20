b17452c24d856c23b24886ea8e7f4b36afa58d91
zengyijie <zengyijie@baibu.la>
2017-12-20 15:02:39 +0800

    setup appdelegate

--- iPhoneX适配Demo/iPhoneX适配Demo/Classes/ViewController.m
+++ iPhoneX适配Demo/iPhoneX适配Demo/Classes/ViewController.m
@@ -17,13 +17,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"viewDidLayoutSubviews --> self.view=%@",self.view);
 }
 
+- (void)viewDidLayoutSubviews{
+    [super viewDidLayoutSubviews];
+    
+    NSLog(@"viewDidLayoutSubviews --> self.view=%@",self.view);
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    NSLog(@"touch");
+}
 
 @end
